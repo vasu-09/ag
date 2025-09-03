01a50a07a7cb13c89855a920bb39ffb92200941f
srinivas-01 <62439147+srinivas-01@users.noreply.github.com>
2025-09-03 08:51:27 +0530

    123

---
+++ src/main/java/com/om/api_gateway/GatewaySecurityConfig.java
@@ -0,0 +1,24 @@
+package com.om.api_gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class GatewaySecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers("/auth/**", "/public/**").permitAll()
+                        .anyExchange().authenticated())
+                .build();
+    }
+}
